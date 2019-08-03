cecd98f8ec2b2e537a498de13ee8b33b3c46fb01
idkup <ratsrotmg@gmail.com>
2019-08-03 02:33:09 -0400

    now works as an applet

--- app.py
+++ app.py
@@ -5,21 +5,40 @@ import numpy as np
 from PIL import Image
 import re
 import json
+import tkinter as tk
 
 
 class EncounterTracker:
-    def __init__(self):
-        bx = pyautogui.locateOnScreen('files/vs.png')
-        self.bx = bx
+    def __init__(self, master):
+        self.bx = None
         self.lastMon = ''
         self.count = 0
-        with open('files/encounters.json', 'w+') as f:
+        with open('files/encounters.json', 'r') as f:
             try:
                 self.encounters = json.load(f)
             except json.decoder.JSONDecodeError:
                 self.encounters = {}
             f.close()
         print(self.encounters)
+        frame = tk.Frame()
+        frame.pack(side=tk.BOTTOM)
+
+        self.calibrate = tk.Button(frame, text="Calibrate", command=self.reset)
+        self.calibrate.pack(side=tk.LEFT)
+        self.exit = tk.Button(frame, text="Exit", command=frame.quit)
+        self.exit.pack(side=tk.RIGHT)
+
+        self.startTracking = tk.Button(root, text="Start Tracking", fg="WHITE", bg="GREEN", command=self.main)
+        self.startTracking.pack(side=tk.LEFT)
+
+        self.encounterBox = tk.Listbox(root)
+        self.encounterBox.pack(side=tk.TOP)
+        for k, v in self.encounters.items():
+            self.encounterBox.insert(tk.END, f"{k}: {v}")
+
+    def reset(self):
+        bx = pyautogui.locateOnScreen('files/vs.png')
+        self.bx = bx
 
     def main(self):
         sc = pyautogui.screenshot().crop((self.bx.left, self.bx.top, self.bx.left + 250, self.bx.top + self.bx.height))
@@ -48,17 +67,19 @@ class EncounterTracker:
                 with open('files/encounters.json', 'w+') as f:
                     json.dump(self.encounters, f)
                     f.close()
+                self.encounterBox.delete(0, tk.END)
+                for k, v in self.encounters.items():
+                    self.encounterBox.insert(tk.END, f"{k}: {v}")
             self.count += 1
         self.lastMon = t
-        time.sleep(2)
+        root.after(2000, a.main)
 
 
 pytesseract.pytesseract.tesseract_cmd = r'ocr\tesseract.exe'
 with open('files/pokedex.json', 'r') as dexfile:
     dex = json.load(dexfile)
     dexfile.close()
-a = EncounterTracker()
-while True:
-    for i in range(10):
-        a.main()
-    print(a.encounters)
+root = tk.Tk()
+root.title("PRO Farm Assistant")
+a = EncounterTracker(root)
+root.mainloop()
